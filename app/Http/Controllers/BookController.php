7f7388f460380a1c73fb6d7266f37857ce494b1d
Cornelius <cornelius.udeh@yahoo.com>
2022-03-23 13:22:06 +0100

    Adds the books endpoints

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,8 +2,17 @@
 
 namespace App\Http\Controllers;
 
-class ExampleController extends Controller
+use Throwable;
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use App\Services\BookService;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Log;
+use App\Services\FormatResponseService;
+
+class BookController extends Controller
 {
+    use BookService, FormatResponseService;
     /**
      * Create a new controller instance.
      *
@@ -16,7 +25,7 @@ class ExampleController extends Controller
 
     public function index()
     {
-        //
+        return $this->allBooks() !== false ? response($this->format('success', $this->allBooks()), 200) : response($this->format('error'), 500);
     }
 
     public function find()
@@ -24,14 +33,57 @@ class ExampleController extends Controller
         //
     }
 
-    public function storeComments()
+    public function storeComments(Request $request, $book)
     {
-        //
+        $this->validate($request, [
+            'comment' => 'required|string|max:500'
+        ]);
+
+        try{
+            
+            if($comment = Comment::create([
+                'comment' => $request->comment,
+                'ip_address' => $request->ip(),
+                'book_id' => $book,
+            ]))
+            {
+                return response($this->format('success', $comment->toArray()), 201);
+            }
+
+        } catch(Throwable $th)
+        {
+            Log::debug('StoreCommentFailed', [
+                'line' => __LINE__,
+                'file' => __FILE__,
+                'exception' => $th->getMessage()
+            ]);
+
+            return response($this->format('error'), 500);
+        }
     }
 
-    public function comments()
+    public function comments($book)
     {
-        //
+        try{
+            $comments = Comment::where('book_id', $book)->orderBy('created_at', 'DESC')->get();
+
+            if($comments->isNotEmpty())
+            {
+                return response($this->format('success', $comments->toArray()), 200);
+            }
+
+            return response($this->format('success', $comments->toArray()), 404);
+
+        } catch(Throwable $th)
+        {
+            Log::debug('FetchCommentsFailed', [
+                'line' => __LINE__,
+                'file' => __FILE__,
+                'exception' => $th->getMessage()
+            ]);
+
+            return response($this->format('error'), 500);
+        }
     }
 
     public function characters()
--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -1,10 +1,47 @@
 <?php
 
 namespace App\Services;
+use GuzzleHttp\Client;
+use App\Models\Comment;
+use Illuminate\Support\Facades\Log;
 
 trait BookService{
-    public function all()
+
+    use APIService;
+
+    public function allBooks()
     {
+        $books = [];
+        // Hello
+        // https://www.anapioficeandfire.com/api/books?pageSize=20
+
+        if($response = $this->getIceAndFire('books', [
+            'pageSize' => 20
+        ]))
+        {
+            for($key = 0; $key < count($response['body']); $key++){
+
+                $book_id = substr(strrchr($response['body'][$key]['url'],"/"), 1);
+
+                $books[$key]['id'] = $book_id;
+                $books[$key]['name'] = $response['body'][$key]['name'];
+                $books[$key]['authors'] = $response['body'][$key]['authors'];
+                $books[$key]['released'] = $response['body'][$key]['released'];
+                $books[$key]['commentCount'] = Comment::where('book_id', $book_id)->count();
+                
+            }
+
+            $releaseDates = array_column($books, 'released');
+            
+            // array_multisort
+            array_multisort($releaseDates, SORT_ASC, $books);
+    
+            return $books;
+        }
+
+        return false;
         
+
     }
+
 }
